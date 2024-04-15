42fff8fea1df1fc8a81b66cbaefce2c20e7a0347
correlate design Inc <info@correlate.design>
2024-04-15 15:58:54 +0900

    LINE表示変更

--- parts-line.php
+++ parts-line.php
@@ -1,1 +1,5 @@
-<a class="btn_line" href="https://lin.ee/sKUoZ12X"><img src="https://scdn.line-apps.com/n/line_add_friends/btn/ja.png" alt="友だち追加" height="36" border="0"></a>
+<a class="btn_line" href="https://lin.ee/sKUoZ12X">
+  <!--<img src="https://scdn.line-apps.com/n/line_add_friends/btn/ja.png" alt="友だち追加" height="36" border="0">-->
+  <img src="<?php bloginfo('template_url'); ?>/img/index/bnr_line.png" class="hidden-sp" alt="友だち追加" height="36" border="0">
+  <img src="<?php bloginfo('template_url'); ?>/img/index/bnr_line_sp.png" class="hidden-pc hidden-tab" alt="友だち追加" height="36" border="0">
+</a>
